16d59673894b318299290a6769b972493f5ae5f3
Håkon V. Treider <haakonvt@Haakons-MacBook-Pro.local>
2016-04-27 16:52:02 -0400

    Added test Forward Euler vs Midpoint

--- travelling_ants.py
+++ travelling_ants.py
@@ -1,10 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-dt = 0.001
+dt = 0.1
 v  = 1
 N  = int(round(1/dt))
-a = 1; b = 1/np.sqrt(2) # Values for exact solution
 
 r1 = np.zeros((N,2))
 r2 = np.zeros((N,2))
@@ -13,6 +12,7 @@ r4 = np.zeros((N,2))
 t  = np.linspace(0,dt*N-dt,N)
 
 """# Exact solution for r1 NOT FINISHED
+a = 1; b = 1/np.sqrt(2) # Values for exact solution
 rex = a*np.exp(-b*t)*np.cos(t)
 rey = a*np.exp(-b*t)*np.sin(t)
 
@@ -26,21 +26,32 @@ r2[0] = np.array([1,0])
 r3[0] = np.array([1,1])
 r4[0] = np.array([0,1])
 
+# Create separate solution with Forward Euler
+r1fe = np.zeros((N,2))
+r2fe = np.zeros((N,2))
+r3fe = np.zeros((N,2))
+r4fe = np.zeros((N,2))
+r1fe[0] = np.array((0,0))
+r2fe[0] = np.array([1,0])
+r3fe[0] = np.array([1,1])
+r4fe[0] = np.array([0,1])
+
 for i in range(N-1):
-    """ # Forward Euler, first order
-    r1r2 = (r2[i]-r1[i])/(np.linalg.norm(r2[i]-r1[i]))
-    r2r3 = (r3[i]-r2[i])/(np.linalg.norm(r3[i]-r2[i]))
-    r3r4 = (r4[i]-r3[i])/(np.linalg.norm(r4[i]-r3[i]))
-    r4r1 = (r1[i]-r4[i])/(np.linalg.norm(r1[i]-r4[i]))
+    # -----------------------------
+    # Forward Euler, first order
+    r1r2 = (r2fe[i]-r1fe[i])/(np.linalg.norm(r2fe[i]-r1fe[i]))
+    r2r3 = (r3fe[i]-r2fe[i])/(np.linalg.norm(r3fe[i]-r2fe[i]))
+    r3r4 = (r4fe[i]-r3fe[i])/(np.linalg.norm(r4fe[i]-r3fe[i]))
+    r4r1 = (r1fe[i]-r4fe[i])/(np.linalg.norm(r1fe[i]-r4fe[i]))
 
-    r1[i+1] = r1[i] + r1r2*v*dt
-    r2[i+1] = r2[i] + r2r3*v*dt
-    r3[i+1] = r3[i] + r3r4*v*dt
-    r4[i+1] = r4[i] + r4r1*v*dt
+    r1fe[i+1] = r1fe[i] + r1r2*v*dt
+    r2fe[i+1] = r2fe[i] + r2r3*v*dt
+    r3fe[i+1] = r3fe[i] + r3r4*v*dt
+    r4fe[i+1] = r4fe[i] + r4r1*v*dt
+    # -----------------------------
 
-    """
-
-    # Runge Kutta of seconds order aka midpoint method
+    # ----------------------------------------
+    # Runge Kutta of second order aka midpoint method
     r1r2 = (r2[i]-r1[i])/(np.linalg.norm(r2[i]-r1[i])) # Direction, unit vector
     r2r3 = (r3[i]-r2[i])/(np.linalg.norm(r3[i]-r2[i]))
     r3r4 = (r4[i]-r3[i])/(np.linalg.norm(r4[i]-r3[i]))
@@ -60,15 +71,8 @@ for i in range(N-1):
     r2[i+1] = r2[i] + 0.5*dt*(r2r3 + k2r2r3)*v
     r3[i+1] = r3[i] + 0.5*dt*(r3r4 + k2r3r4)*v
     r4[i+1] = r4[i] + 0.5*dt*(r4r1 + k2r4r1)*v
+    # ----------------------------------------
 
-
-if raw_input("Hit 'enter' to skip plot") != '':
-    plt.plot(r1[:,0], r1[:,1],'o-')
-    plt.plot(r2[:,0], r2[:,1],'o-')
-    plt.plot(r3[:,0], r3[:,1],'o-')
-    plt.plot(r4[:,0], r4[:,1],'o-')
-    plt.legend(["r1","r2","r3","r4"])
-    plt.show()
 
 def integratooor(r):
     dr = 0
@@ -92,14 +96,27 @@ def radial_velocity(r,dt):
 
 #rtotal = integratooor(r1)
 #print "The magical distance is", rtotal
-
 vr = radial_velocity(r1,dt)
-plt.plot(t,vr)
-plt.show()
-#print vr
+
+if raw_input("Hit 'enter' to skip plot") != '':
+    plt.plot(r1[:,0], r1[:,1],'o-')
+    plt.plot(r1fe[:,0], r1fe[:,1],'o-')
+    plt.plot(r2[:,0], r2[:,1],'o-')
+    plt.plot(r3[:,0], r3[:,1],'o-')
+    plt.plot(r4[:,0], r4[:,1],'o-')
+    plt.legend(["r1","r1 FE","r2","r3","r4"])
+    plt.show()
+
+    plt.plot(t,vr)
+    plt.title("Magnitude of radial velocity (i.e. towards center)")
+    plt.xlabel("Time")
+    plt.ylabel("Velocity")
+    plt.show()
+
 vr_avgN20pst = sum(vr[0:int(N/5)])/int(N/5) # Average over first 20% of vr values
 rmid         = np.sqrt(2*0.5**2)
 
+print "-------------------------"
 print "As we ca see, the radial velocity stays constant,"
 print "(except for numerical errors near the end), "
 print "and the distance to middle, rm = sqrt(2*(0.5)^2), we have"
